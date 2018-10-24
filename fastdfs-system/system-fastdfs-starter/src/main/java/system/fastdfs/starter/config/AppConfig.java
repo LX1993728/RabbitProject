e6632a88dade66858251563a8059c4db4f0a0a39
LX1993728 <2652790899@qq.com>
2018-10-24 09:54:09 +0800

    ++++++修改前端代码 js获取上下文路径++++++++++

--- fastdfs-system/system-fastdfs-starter/src/main/java/system/fastdfs/starter/config/AppConfig.java
+++ fastdfs-system/system-fastdfs-starter/src/main/java/system/fastdfs/starter/config/AppConfig.java
@@ -1,11 +1,12 @@
 package system.fastdfs.starter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppConfig {
 
-    private String fdfsUrl = "192.168.1.150";
+    private String fdfsUrl = "20.1.0.150";
 
 
     public String getFdfsUrl() {
--- heartbeat-monitor/monitor-system/src/main/resources/static/js/common.js
+++ heartbeat-monitor/monitor-system/src/main/resources/static/js/common.js
@@ -2,22 +2,22 @@ $(function (){
 	get_head("#common_head",'./common_head.html')
 
       // 获取天气API
-    $.get("https://free-api.heweather.com/s6/weather/forecast?location=" + returnCitySN['cip'] + "&key=02f450fa55394f8d868aa2a1d1dc0772", function (data, status) {
-        if (data.HeWeather6[0].daily_forecast) {
-            let iconAddress = data.HeWeather6[0].daily_forecast[0].cond_code_d
-            $(".left_con .weather .icon").css({
-                //'background-image': 'url(https://cdn.heweather.com/cond_icon/' + iconAddress + '.png)'
-                'background-image': 'url(./images/weather/' + iconAddress + '.png)'
-            })
-            $(".left_con .weather .weather_des").html(data.HeWeather6[0].daily_forecast[0].cond_txt_d)
-        } else {
-            $(".left_con .weather").html('暂无数据')
-            $(".left_con .weather").css({
-                'line-height': '90px',
-                'font-size': '16px'
-            })
-        }
-    })
+    // $.get("https://free-api.heweather.com/s6/weather/forecast?location=" + returnCitySN['cip'] + "&key=02f450fa55394f8d868aa2a1d1dc0772", function (data, status) {
+    //     if (data.HeWeather6[0].daily_forecast) {
+    //         let iconAddress = data.HeWeather6[0].daily_forecast[0].cond_code_d
+    //         $(".left_con .weather .icon").css({
+    //             //'background-image': 'url(https://cdn.heweather.com/cond_icon/' + iconAddress + '.png)'
+    //             'background-image': 'url(./images/weather/' + iconAddress + '.png)'
+    //         })
+    //         $(".left_con .weather .weather_des").html(data.HeWeather6[0].daily_forecast[0].cond_txt_d)
+    //     } else {
+    //         $(".left_con .weather").html('暂无数据')
+    //         $(".left_con .weather").css({
+    //             'line-height': '90px',
+    //             'font-size': '16px'
+    //         })
+    //     }
+    // })
 
     // 判断当前用户是否是登录状态,如果不是则退回到登录页
     $.ajaxSetup({
@@ -35,7 +35,21 @@ $(function (){
         $(this).parents(".info_dialoge").hide();
     })
 });
-	var baseServerUrl = "http://192.168.1.150:8011";
+
+function getRootPath(){
+    //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
+    var curWwwPath=window.document.location.href;
+    //获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
+    var pathName=window.document.location.pathname;
+    var pos=curWwwPath.indexOf(pathName);
+    //获取主机地址，如： http://localhost:8083
+    var localhostPaht=curWwwPath.substring(0,pos);
+    //获取带"/"的项目名，如：/uimcardprj
+    var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
+    return(localhostPaht+projectName);
+}
+
+	var baseServerUrl = getRootPath();
 
 // 加载公共头部方法
 function get_head (html_container,html_name){
